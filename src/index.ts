9c38f160ab21c2e5d817a10ef7f9056878df2c1b
Eddie Hedges <eddiehedges@gmail.com>
2019-04-10 01:29:30 -0500

    Structured file for AWS Lambda

--- src/index.ts
+++ src/index.ts
@@ -58,97 +58,100 @@ const allPromises = [
     ...orgPromises,
 ]
 
-Promise.all(allPromises)
-    .then(results => {
-        if (!results.length) {
-            throw Error('Empty results array upon promises being resolved')
-        }
+exports.handler = () =>
+    Promise.all(allPromises)
+        .then(results => {
+            if (!results.length) {
+                throw Error('Empty results array upon promises being resolved')
+            }
 
-        const yesterday = moment.utc().subtract(1, 'day')
-        const allResults: any[] = []
-        results.forEach(r => {
-            const userResults = r
-                .filter((d: any) => {
-                    if (d.actor.login !== username) {
-                        return false
+            const yesterday = moment.utc().subtract(1, 'day')
+            const allResults: any[] = []
+            results.forEach(r => {
+                const userResults = r
+                    .filter((d: any) => {
+                        if (d.actor.login !== username) {
+                            return false
+                        }
+
+                        const createdAtMoment = moment(d.created_at)
+                        return yesterday.diff(createdAtMoment, 'days') < 7
+                    })
+                    .sort(function(a: any, b: any) {
+                        // Turn your strings into dates, and then subtract them
+                        // to get a value that is either negative, positive, or zero.
+                        return (
+                            new Date(b.created_at).getTime() -
+                            new Date(a.created_at).getTime()
+                        )
+                    })
+                userResults.forEach((rr: any) => {
+                    // only add unique ids to results
+                    if (allResults.findIndex(ar => ar.id === rr.id) === -1) {
+                        // TODO: Add configurability for these results
+
+                        // TODO: Think about taking into account PRs that were opened, but later closed
+                        // and are merged, but were not merged by the author of the PR
+
+                        // TODO: Break out logic here into functions describing the different
+                        // conditional that allow a resutls to be added
+
+                        // Events that should be included:
+                        // - PRs that were closed and merged (done)
+                        // - PRs that were opened and are now closed and merged, but weren't merged by opener
+                        // - Commits made to master branches
+                        // - Repository was started
+                        // - Branch created???
+                        // - Checkout https://developer.github.com/v3/activity/events/types/ to discover other important ones
+                        if (
+                            rr.type === 'PullRequestEvent' &&
+                            rr.payload.action === 'closed' &&
+                            rr.payload.pull_request.merged === true
+                        ) {
+                            allResults.push(rr)
+                        }
                     }
-
-                    const createdAtMoment = moment(d.created_at)
-                    return yesterday.diff(createdAtMoment, 'days') < 7
                 })
-                .sort(function(a: any, b: any) {
-                    // Turn your strings into dates, and then subtract them
-                    // to get a value that is either negative, positive, or zero.
-                    return (
-                        new Date(b.created_at).getTime() -
-                        new Date(a.created_at).getTime()
-                    )
-                })
-            userResults.forEach((rr: any) => {
-                // only add unique ids to results
-                if (allResults.findIndex(ar => ar.id === rr.id) === -1) {
-                    // TODO: Add configurability for these results
-
-                    // TODO: Think about taking into account PRs that were opened, but later closed
-                    // and are merged, but were not merged by the author of the PR
-
-                    // TODO: Break out logic here into functions describing the different
-                    // conditional that allow a resutls to be added
-
-                    // Events that should be included:
-                    // - PRs that were closed and merged (done)
-                    // - PRs that were opened and are now closed and merged, but weren't merged by opener
-                    // - Commits made to master branches
-                    // - Repository was started
-                    // - Branch created???
-                    // - Checkout https://developer.github.com/v3/activity/events/types/ to discover other important ones
-                    if (
-                        rr.type === 'PullRequestEvent' &&
-                        rr.payload.action === 'closed' &&
-                        rr.payload.pull_request.merged === true
-                    ) {
-                        allResults.push(rr)
-                    }
+            })
+            const simplifiedResults = allResults.map(r => {
+                const pr = r.payload.pull_request
+                return {
+                    url: pr.html_url,
+                    title: pr.title,
+                    body: pr.body,
+                    createdAt: moment(pr.created_at).format(
+                        'ddd, MMM Do YYYY, h:mm a'
+                    ),
                 }
             })
-        })
-        const simplifiedResults = allResults.map(r => {
-            const pr = r.payload.pull_request
-            return {
-                url: pr.html_url,
-                title: pr.title,
-                body: pr.body,
-                createdAt: moment(pr.created_at).format(
-                    'ddd, MMM Do YYYY, h:mm a'
-                ),
+            const payloadText =
+                '*My last week on GitHub*\n\n' +
+                simplifiedResults
+                    .map(r => {
+                        return `<${r.url}|${r.title}> (${r.createdAt})\n${
+                            r.body
+                        }`
+                    })
+                    .join('\n\n')
+
+            if (typeof slackWebhookUrl === 'undefined') {
+                console.log(payloadText)
+            } else {
+                axios
+                    .post(slackWebhookUrl, {
+                        text: payloadText,
+                    })
+                    .then(_ => {
+                        console.log('Successfully posted to slack!')
+                    })
+                    .catch(rejectionReason =>
+                        console.error(
+                            'Error POSTing results to slack webhook',
+                            rejectionReason
+                        )
+                    )
             }
         })
-        const payloadText =
-            '*My last week on GitHub*\n\n' +
-            simplifiedResults
-                .map(r => {
-                    return `<${r.url}|${r.title}> (${r.createdAt})\n${r.body}`
-                })
-                .join('\n\n')
-
-        if (typeof slackWebhookUrl === 'undefined') {
-            console.log(payloadText)
-        } else {
-            axios
-                .post(slackWebhookUrl, {
-                    text: payloadText,
-                })
-                .then(res => {
-                    console.log('res', res.data, res.status, res.statusText)
-                })
-                .catch(rejectionReason =>
-                    console.error(
-                        'Error POSTing results to slack webhook',
-                        rejectionReason
-                    )
-                )
-        }
-    })
-    .catch(rejectionReason => {
-        console.error('Error resolving all promises', rejectionReason)
-    })
+        .catch(rejectionReason => {
+            console.error('Error resolving all promises', rejectionReason)
+        })
